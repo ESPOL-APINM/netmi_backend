68a5370c3205b1fb06a34ab7becc8618ff2347f4
Alex Cordova <33164007+alealcor@users.noreply.github.com>
2021-07-06 02:17:47 -0500

    Corrections

--- src/gpapi.py
+++ src/gpapi.py
@@ -1,12 +1,14 @@
 from genie import testbed
 
+
 class gpapi(object):
-    def __init__(self, testbed_file=None,device_name=None):
+    def __init__(self, testbed_file=None, device_name=None):
         test = testbed.load(testbed_file)
         self.device = test.devices[device_name]
 
     def connect(self):
-        self.device.connect(init_exec_commands=[], init_config_commands=[], log_stdout=False)
+        self.device.connect(init_exec_commands=[],
+                            init_config_commands=[], log_stdout=False)
 
     def disconnect(self):
         self.device.disconnect()
--- src/main.py
+++ src/main.py
@@ -1,57 +1,79 @@
-from flask import Flask,json,request
+from flask import Flask, json, request
 import yaml
 from gpapi import gpapi
+
 app = Flask(__name__)
 
-@app.route('/getvrf',methods=['POST'])
+
+def shutdown_server():
+    func = request.environ.get('werkzeug.server.shutdown')
+    if func is None:
+        raise RuntimeError('Not running with the Werkzeug Server')
+    func()
+
+
+@app.route('/')
+def status():
+    return 'The server is running... [OK!]'
+
+
+@app.route('/restart')
+def shutdown():
+    shutdown_server()
+    app.run()
+    return 'Restarting server complete.'
+
+
+@app.route('/getvrf', methods=['POST'])
 def getvrf():
     json_data = request.get_json()
     name = json_data["name"]
-    connection =  gpapi("./testbed.yaml",name)
+    connection = gpapi("./testbed.yaml", name)
     parse = connection.get_vrf_detail()
-    data = {"data":parse} 
+    data = {"data": parse}
     response = app.response_class(response=json.dumps(data),
                                   status=200,
                                   mimetype='application/json')
     return response
 
-@app.route('/getvrfyaml',methods=['POST'])
+
+@app.route('/getvrfyaml', methods=['POST'])
 def getvrfyaml():
-    
-    connection =  gpapi("./testbed.yaml","RIOS1")
+
+    connection = gpapi("./testbed.yaml", "RIOS1")
     parse = connection.get_vrf_detail()
     vrfs = {}
-    for i in parse: 
-        vrfs[i]={"vrf_name" : i}   
-        dict_target = parse[i] 
+    for i in parse:
+        vrfs[i] = {"vrf_name": i}
+        dict_target = parse[i]
         print(dict_target)
-        dict_routes=dict_target["address_family"]["ipv4"]
-             
+        dict_routes = dict_target["address_family"]["ipv4"]
+
         if "route_distinguisher" in dict_target.keys():
-            route_distinguisher=dict_target["route_distinguisher"]
+            route_distinguisher = dict_target["route_distinguisher"]
             vrfs[i]["vrf_rd"] = route_distinguisher
         else:
-            doc=i+" no tiene route_distinguisher"
-            s=400
-            break 
+            doc = i+" no tiene route_distinguisher"
+            s = 400
+            break
 
         if "route_targets" not in dict_routes.keys():
-            doc=i+" no tiene route_targets"
+            doc = i+" no tiene route_targets"
             print("no route_targets")
-            s=400
-            break 
-        route_targets = dict_routes["route_targets"] 
+            s = 400
+            break
+        route_targets = dict_routes["route_targets"]
 
         if "description" in dict_target.keys():
-            description=dict_target["description"]
+            description = dict_target["description"]
             vrfs[i]["vrf_dct"] = description
 
         if "import_route_map" in dict_routes.keys():
-            route_imap = dict_routes["import_route_map"] 
+            route_imap = dict_routes["import_route_map"]
             vrfs[i]["vrf_imap"] = route_imap
 
         if "export_route_map" in dict_routes.keys():
-            route_emap = dict_routes["export_route_map"] 
+            route_emap = dict_routes["export_route_map"]
             vrfs[i]["vrf_emap"] = route_emap
 
         if "routing_table_limit" in dict_routes.keys():
@@ -61,32 +83,33 @@ def getvrfyaml():
             vrfs[i]["vrf_lper"] = route_limit_percent
             vrfs[i]["vrf_ln"] = route_limit_number
         print("n")
-        n=1
+        n = 1
         for rt in route_targets:
-            dict_rt= route_targets.get(rt)
-            rttype=dict_rt.get("rt_type")
-            if rttype=="both":
-                vrfs[i]["vrf_ex"+str(n)]=dict_rt.get("route_target")
-                vrfs[i]["vrf_im"+str(n)]=dict_rt.get("route_target")
-            elif rttype=="export":
-                vrfs[i]["vrf_ex"+str(n)]=dict_rt.get("route_target")
-            elif rttype=="import":
-                vrfs[i]["vrf_im"+str(n)]=dict_rt.get("route_target")
-            n=n+1
+            dict_rt = route_targets.get(rt)
+            rttype = dict_rt.get("rt_type")
+            if rttype == "both":
+                vrfs[i]["vrf_ex"+str(n)] = dict_rt.get("route_target")
+                vrfs[i]["vrf_im"+str(n)] = dict_rt.get("route_target")
+            elif rttype == "export":
+                vrfs[i]["vrf_ex"+str(n)] = dict_rt.get("route_target")
+            elif rttype == "import":
+                vrfs[i]["vrf_im"+str(n)] = dict_rt.get("route_target")
+            n = n+1
 
     doc = yaml.dump(vrfs)
-    s=200
-    #except:
+    s = 200
+    # except:
     #    doc=""
     #    s=400
 
-    #data = {"data":parse} # Your data in JSON-serializable type
+    # data = {"data":parse} # Your data in JSON-serializable type
     response = app.response_class(response=doc,
                                   status=s,
                                   mimetype='text/yaml')
     return response
 
-@app.route('/addtoyaml',methods=['POST'])
+
+@app.route('/addtoyaml', methods=['POST'])
 def addYaml():
     try:
         json_data = request.get_json()
@@ -97,42 +120,43 @@ def addYaml():
         password = json_data["password"]
         secret = json_data["secret"]
         new_yaml_data_dict = {
-            name:{
-                'connections':{
-                    'cli':{
+            name: {
+                'connections': {
+                    'cli': {
                         'ip': ip,
                         'protocol': 'ssh -c aes128-cbc'
                     }
                 },
-                'credentials':{
-                    'default':{
+                'credentials': {
+                    'default': {
                         'username': username,
                         'password': password
-                    },'enable':{
-                        'password': secret                }
+                    }, 'enable': {
+                        'password': secret}
                 },
                 'os': os,
                 'type': os
             }
         }
-        with open('./testbed.yaml','r') as yamlfile:
-            testbedyaml = yaml.safe_load(yamlfile) # Note the safe_load
+        with open('./testbed.yaml', 'r') as yamlfile:
+            testbedyaml = yaml.safe_load(yamlfile)  # Note the safe_load
             testbedyaml['devices'].update(new_yaml_data_dict)
 
         if testbedyaml:
-            with open('testbed.yaml','w') as yamlfile:
-                yaml.safe_dump(testbedyaml, yamlfile) 
-        d="OK"
-        s=200
+            with open('testbed.yaml', 'w') as yamlfile:
+                yaml.safe_dump(testbedyaml, yamlfile)
+        d = "OK"
+        s = 200
     except:
-        d="Error addYaml"
-        s=400
-    
-    data = {"data":d}
+        d = "Error addYaml"
+        s = 400
+
+    data = {"data": d}
     response = app.response_class(response=json.dumps(data),
                                   status=s,
                                   mimetype='application/json')
     return response
-    
+
+
 if __name__ == "__main__":
     app.run(debug=True)
