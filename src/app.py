61d6729d4c4f0b18184a20a2d2923fca18e0cb58
Alex Cordova <33164007+alealcor@users.noreply.github.com>
2021-07-06 00:35:51 -0500

    New schema

--- src/app.py
+++ src/app.py
@@ -1,6 +1,23 @@
 from flask import Flask
+from flask import request
+
 app = Flask(__name__)
+
+
+def shutdown_server():
+    func = request.environ.get('werkzeug.server.shutdown')
+    if func is None:
+        raise RuntimeError('Not running with the Werkzeug Server')
+    func()
+
+
+@app.route('/shutdown')
+def shutdown():
+    shutdown_server()
+    app.run()
+    return 'Server restarting now...'
+
 
 @app.route('/')
 def hello_world():
-    return 'Hola, Caro! Ya esta listo el entorno de desarrollo bb'
+    return 'Hola, Caro! Ya esta listo el entorno de desarrollo abb'
